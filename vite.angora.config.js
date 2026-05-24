7486fbe0383f6fdb1546e081d275a207282cfffb
Othniel Obasi <othielobasi@gmail.com>
2026-05-24 02:48:04 +0100

    Fix Angora frontend asset base

--- vite.angora.config.js
+++ vite.angora.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/angora-app/',
   root: 'src/dashboard/frontend',
   build: {
     outDir: '../public/angora-app',
